92b0e659600642a175610ac5d72b785af36000d4
Kanljung, Christofer <christofer.kanljung@volvocars.com>
2022-02-11 16:37:11 +0100

    Added basic alu class

---
+++ src/main/scala/alu/alu.scala
@@ -0,0 +1,33 @@
+// See README.md for license details.
+
+package alu
+
+import chisel3._
+import chisel3.util._
+
+/**
+  * RISC-V R32I ISA ALU
+  */
+class alu extends Module {
+  val io = IO(new Bundle {
+    val value1        = Input(UInt(16.W))
+    val value2        = Input(UInt(16.W))
+    val loadingValues = Input(Bool())
+    val outputGCD     = Output(UInt(16.W))
+    val outputValid   = Output(Bool())
+  })
+
+  val x  = Reg(UInt())
+  val y  = Reg(UInt())
+
+  when(x > y) { x := x - y }
+    .otherwise { y := y - x }
+
+  when(io.loadingValues) {
+    x := io.value1
+    y := io.value2
+  }
+
+  io.outputGCD := x
+  io.outputValid := y === 0.U
+}
